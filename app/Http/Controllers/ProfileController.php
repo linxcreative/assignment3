f8870621b8eb4eba5951f781e004ab4762662a3e
linx.creative@gmail.com <linx.creative@gmail.com>
2024-06-19 23:11:42 +0600

    Asiignment 3 For ostad Module
    send a get request to /profile route and return a json array

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+
+        $name = "Donal Trump";
+        $age = "75";
+
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+
+        $response = new Response(json_encode($data));
+        $response->withCookie(cookie(
+            $cookieName,
+            $cookieValue,
+            $cookieMinutes,
+            $cookiePath,
+            $cookieDomain,
+            $cookieSecure,
+            $cookieHttpOnly
+        ));
+
+        return $response->setStatusCode(200)
+                        ->header('Content-Type', 'application/json');
+    }
+}
+
+
